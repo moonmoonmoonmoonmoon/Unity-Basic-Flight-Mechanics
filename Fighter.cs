a11b62b9f9b29bd586924e2f1be0c2bd0c73d2c0
jorzales <jorzales@live.esu.edu>
2022-02-06 07:21:34 -0500

    Made changes to Stall() and ForwardSpeed() method for the Fighter.cs script.

--- Fighter.cs
+++ Fighter.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Fighter : Aircraft
 {
-    // Will serialize these at a later time
+
     // Controlling these values would be better on the UI
     private Rigidbody rb;
-    private float speed = 1.0f, weight = 50000, lift = 1.0f;
-    private float breaking = 10.0f;
-    private float dragMax = 0.8f, manuverMax = 5, manuverVal = 0.1f;
-    [SerializeField] private float pitchStrength = 3.0f, yawStrength = 8.0f, rollStrength = 1.0f;
+    private float lift = 1.0f;
+    private float manuverMax = 5, manuverVal = 0.1f;
+
+    // serialized fields:
+    // - need to optimize these in the future
+    // - manipulating these values on the editor may be confusing
+    // - however, manipulating these are easier now than before
+    [SerializeField] private float 
+        pitchStrength = 3.0f, 
+        yawStrength = 8.0f, 
+        rollStrength = 1.0f,
+        weight = 50000,
+        breaking = 10.0f,
+        dragMax = 5.0f,
+        speedLift = 60.0f,
+        cruisingLift = 13.0f,
+        speed = 0.25f,
+        cruisingSpeed = 200.0f;
     public override void Manuver() // combined torque controls
     {
-        // These are unique to an Airliner or Large Aircraft
+        // These are unique to a derived aircraft class
         Pitch(manuverVal);
         Yaw(manuverVal);
         Roll(manuverVal);
@@ -58,29 +72,58 @@ public class Fighter : Aircraft
     public override void ForwardSpeed()
     {
         // forward acceleration
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            
+            
+            rb.AddForce(transform.forward * 1 / speed, ForceMode.VelocityChange);
+            rb.AddForce(transform.up * lift / speedLift, ForceMode.VelocityChange);
+            if (rb.velocity.magnitude >= 80.0f)
+            {
+                // speed limit when using 'afterburner'
+                rb.velocity = rb.velocity.normalized * 80.0f;
+            }
+
+        }
+
+        // air brakes
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(transform.forward * speed * 0.5f, ForceMode.VelocityChange);
-            rb.AddForce(transform.up * lift / 100.0f, ForceMode.VelocityChange);
-            Stall();
 
+            rb.AddForce(transform.forward * 1 / breaking, ForceMode.VelocityChange);
+            Stall();
+            rb.AddForce(transform.up * lift / cruisingLift, ForceMode.VelocityChange);
+            while (cruisingSpeed > -10 && cruisingSpeed < 10)
+            {
+                // decrease forward acceleration when braking
+                cruisingSpeed = cruisingSpeed - 1;
+            }
         }
 
-        // breaking
-        if (Input.GetKey(KeyCode.F))
-        {
-            rb.AddForce(transform.forward * breaking, ForceMode.Impulse);
-            Stall();
-        }
-
-        // cruising
+        // cruising speed
         else
         {
-            rb.AddForce(transform.forward * 0.5f, ForceMode.VelocityChange);
-            rb.AddForce(transform.up * lift / 14.0f, ForceMode.VelocityChange);
-            Stall();
+            rb.AddForce(transform.forward * 1 / cruisingSpeed, ForceMode.VelocityChange);
+            while (cruisingSpeed < 10)
+            {
+                // increase forward acceleration when brakes are released
+                // will optimize the numbers in the future
+                cruisingSpeed = cruisingSpeed + 1 / 1000000;
+                
+            }
 
+            Stall();
+            rb.AddForce(transform.up * lift / cruisingLift, ForceMode.VelocityChange);
+            if (rb.velocity.magnitude >= 80.0f && !Input.GetKey(KeyCode.Space))
+            {
+                // speed limit
+                rb.velocity = rb.velocity.normalized * 80.0f;
+            }
+            
         }
+
+        // debugging an issue with z-axis angle for Stall()
+        //Debug.Log(transform.eulerAngles.z);
 
     }
 
@@ -88,12 +131,22 @@ public class Fighter : Aircraft
     {
         // stall motion if speed is less than a value
         // affects the lift of the aircraft at low speed
-        if (rb.velocity.magnitude < 35.0f && !(rb.velocity.magnitude < 1))
+        // affects the pitch angle of the aircraft
+        if (rb.velocity.magnitude < 30.0f)
         {
-            rb.AddForce(transform.up * 0.0f, ForceMode.VelocityChange);
+            // will need to optimize this in the future
+            // this changes the pitch angle when stalling
+            if ((transform.eulerAngles.z <= 75.0f && transform.eulerAngles.z >= 0.0f) || 
+                !(transform.eulerAngles.z <= 315.0f && transform.eulerAngles.z >= 150.0f ) &&
+                !(transform.eulerAngles.z >= 30.0f && transform.eulerAngles.z <= 150.0f)) 
+            {
+                rb.AddTorque(transform.right * manuverVal / 30, ForceMode.VelocityChange);
+            }
+
+            // decrease lift when aircraft velocity is below a certain value
+            rb.AddForce(transform.up * -1 / 50, ForceMode.VelocityChange);
             Debug.Log("WARNING: STALL!!!");
         }
-
         
     }
 
